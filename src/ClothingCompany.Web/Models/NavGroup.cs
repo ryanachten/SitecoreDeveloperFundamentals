34fb844e65515a680c368bc57a0583972e9a1104
Ryan Achten <ryanachten@gmail.com>
2020-08-19 10:34:22 +1200

    Add nav models and apply to Footer

---
+++ src/ClothingCompany.Web/Models/NavGroup.cs
@@ -0,0 +1,13 @@
+﻿using Sitecore.Data.Items;
+using System.Collections.Generic;
+
+namespace ClothingCompany.Models
+{
+    public class NavGroup
+    {
+        public Item RootItem { get; set; }
+        public string RootUrl { get; set; }
+        public IEnumerable<NavItem> NavItems { get; set; }
+
+    }
+}
---
+++ src/ClothingCompany.Web/Models/NavItem.cs
@@ -0,0 +1,15 @@
+﻿using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClothingCompany.Models
+{
+    public class NavItem
+    {
+        public Item Item { get; set; }
+        public Item Url { get; set; }
+
+    }
+}
